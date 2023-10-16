b21ed706dff0895c471095f860c2c8455fa96073
krystynariabokon@outlook.com <krystynariabokon@outlook.com>
2023-10-16 20:04:09 +1000

    test

---
+++ DDT/script.js
@@ -0,0 +1,8 @@
+/* from https://www.w3schools.com/jsref/met_form_submit.asp*/
+function myFunction() {
+  document.getElementById("create").submit();
+}
+/* from https://www.w3schools.com/jsref/event_onload.asp*/
+function cookiesAlert() {
+  alert("Hi, we are using cookies on out website. By continuing to use Dogs Date Too you consent to it.");
+}
